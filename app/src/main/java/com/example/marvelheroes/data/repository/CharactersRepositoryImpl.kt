3cf629c10707e0e76fc3d9f0467c02f78e73b0df
nickand <gonzalezn775@gmail.com>
2022-06-27 23:20:47 -0500

    Clean code

--- app/src/main/java/com/example/marvelheroes/data/repository/CharactersRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelheroes/data/repository/CharactersRepositoryImpl.kt
@@ -6,7 +6,7 @@ import com.example.marvelheroes.data.model.Resource
 import com.example.marvelheroes.data.network.MarvelAPI
 
 /**
- * Implements @see MercadoLibreAPI
+ * Implements @see MarvelAPI
  */
 class CharactersRepositoryImpl(
     private val marvelAPI: MarvelAPI,
@@ -14,23 +14,6 @@ class CharactersRepositoryImpl(
     private val dtoMapper: CharacterDTOMapper,
     //private val cacheMapper: ProductCacheMapper
 ) : CharactersRepository {
-
-    /**
-     * Will fetch results according to @param query, clear the database and then store the results
-     * as the new cache data.
-     */
-    /*override suspend fun search(query: String): Resource<List<Product?>> {
-        return try {
-            val dtoList = mercadoLibreAPI.search(query).results
-            val domainList = dtoMapper.toDomainList(dtoList)
-            val cacheList = cacheMapper.fromList(domainList)
-            productDAO.deleteAll()
-            productDAO.save(*cacheList.toTypedArray())
-            Resource.Success(domainList)
-        } catch (throwable: Throwable) {
-            Resource.Error(throwable.message ?: "Error fetching result list from service")
-        }
-    }*/
 
     override suspend fun getCharacters(): Resource<List<Character?>> {
         return try {
@@ -44,25 +27,4 @@ class CharactersRepositoryImpl(
             Resource.Error(throwable.message ?: "Error fetching result list from service")
         }
     }
-
-    /**
-     * Will get a Product details from the database cache, in case the data is not complete, the rest
-     * of the information will be fetched from the service.
-     */
-   /* override suspend fun get(productId: String): Resource<Product> {
-        return try {
-            var productCache = productDAO.loadResult(productId)
-            if (productCache.lastUpdated == null) {
-                val productDTO = mercadoLibreAPI.getDetails(productId)
-                val product = dtoMapper.mapToModel(productDTO)
-                productCache = cacheMapper.mapFromModel(product)
-                productDAO.update(productCache)
-                Resource.Success(product)
-            } else {
-                Resource.Success(cacheMapper.mapToModel(productCache))
-            }
-        } catch (throwable: Throwable) {
-            Resource.Error(throwable.message ?: "Error fetching product from database")
-        }
-    }*/
 }
